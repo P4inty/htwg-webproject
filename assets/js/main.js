3e370f0bda71baf7e82db8693b0cdc820e098f71
Martin Wundsam <martin.wundsam@gmail.com>
2019-05-18 19:12:38 +0200

    adjust for mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,20 @@ var carousel = new Vue({
     data: {
         slides: [false, true, true],
         hideMore: true,
+        button: "Mehr anzeigen"
     },
     methods: {
         left: function() {
+            this.hideMore = true;
             this.slides.push(this.slides.shift());
         },
         right: function() {
+            this.hideMore = true;
             this.slides.unshift(this.slides.pop());
+        },
+        showMore: function() {
+            this.hideMore ? this.button = "Weniger anzeigen" : this.button = "Mehr anzeigen";
+            this.hideMore = !this.hideMore;
         }
     }
 });
