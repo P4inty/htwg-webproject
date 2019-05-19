444fbd6a2390bba728f4479cf73fb5239e03856c
Martin Wundsam <martin.wundsam@gmail.com>
2019-05-19 22:48:57 +0200

    add cookie disclaimer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,25 @@ var nav = new Vue({
     },
 });
 
+var disclaimer = new Vue({
+    el: '#disclaimer',
+    data: {
+        show: true,
+    },
+    methods: {
+        accept: function(accept) {
+            $cookies.config('365d');
+            $cookies.set('cookie_disclaimer', accept);
+            this.show = false;
+        }
+    },
+    beforeMount: function() {
+        if($cookies.isKey('cookie_disclaimer')) {
+            this.show = false;
+        }
+    }
+});
+
 var carousel = new Vue({
     el: '#carousel',
     data: {
