bcb01cb4d7b4e7b29b1e0b92c5f37e714bbba78b
Martin Wundsam <martin.wundsam@gmail.com>
2019-05-20 23:10:55 +0200

    outsource carousel-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,26 +23,3 @@ var disclaimer = new Vue({
         }
     }
 });
-
-var carousel = new Vue({
-    el: '#carousel',
-    data: {
-        slides: [false, true, true],
-        hideMore: true,
-        button: "Mehr anzeigen"
-    },
-    methods: {
-        left: function() {
-            this.hideMore = true;
-            this.slides.push(this.slides.shift());
-        },
-        right: function() {
-            this.hideMore = true;
-            this.slides.unshift(this.slides.pop());
-        },
-        showMore: function() {
-            this.hideMore ? this.button = "Weniger anzeigen" : this.button = "Mehr anzeigen";
-            this.hideMore = !this.hideMore;
-        }
-    }
-});
