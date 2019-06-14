60049feddb3a7dec747b64083e49442e0903c8bb
Martin Wundsam <martin.wundsam@gmail.com>
2019-06-14 15:26:08 +0200

    add search function

---
+++ assets/js/search.js
@@ -0,0 +1,44 @@
+class Product {
+    constructor(title, imgLink, link, category) {
+        this.title = title;
+        this.imgLink = imgLink;
+        this.link = link;
+        this.category = category;
+    }
+}
+
+var search = new Vue({
+    el: '#searchbar',
+    data: {
+        showMenu: false,
+        search: '',
+        products: [
+            new Product(
+                'Lactohelp',
+                'assets/img/product_1.png',
+                '#',
+                'Laktose Prävention'
+            ),
+            new Product(
+                'Pollblock',
+                'assets/img/product_2.png',
+                '#',
+                'Pollen'
+            ),
+            new Product(
+                'Hairresist',
+                'assets/img/product_3.png',
+                '#',
+                'Tierhaare'
+            )
+        ],
+    },
+    computed: {
+        searchedProducts: function () {
+            return this.products.filter(product => {
+                return product.title.toLowerCase().includes(this.search.toLowerCase()) ||
+                    product.category.toLowerCase().includes(this.search.toLowerCase());
+            })
+        }
+    }
+});
